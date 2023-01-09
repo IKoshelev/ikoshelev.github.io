a33aebaa0b781a0424b6cce43791b2b5b1b0b11b
Ivan Koshelev <koshelew@gmail.com>
2023-01-09 20:47:13 +0100

    mobx

---
+++ ui-app-architecture-showcases/mobx/precache-manifest.807f4c556095248e64c993f8a6369b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730f676947446fbe894f9c0e798a54fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e268a0bc1d540b0390b",
+    "url": "/static/css/main.bb993cbc.chunk.css"
+  },
+  {
+    "revision": "9e4da1fe67265b51ce75",
+    "url": "/static/js/2.3a7a59cd.chunk.js"
+  },
+  {
+    "revision": "b3e2770e6336acd821638b70a2a55e3e",
+    "url": "/static/js/2.3a7a59cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e268a0bc1d540b0390b",
+    "url": "/static/js/main.d074c8d5.chunk.js"
+  },
+  {
+    "revision": "061edaf03266229903a1",
+    "url": "/static/js/runtime-main.8cb38b00.js"
+  }
+]);
